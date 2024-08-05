f13f1b67451fc5892741b17739a84df1f3ee162b
Prabin <prabinroy1@gmail.com>
2024-08-05 14:47:00 +0530

    test the server with only ca

--- server.js
+++ server.js
@@ -72,8 +72,8 @@ app.use('/', searchRoute);
 app.use('/', otpRoute);
 // Read the SSL certificate and key
 const options = {
-  key: fs.readFileSync('/etc/nginx/ssl/origin_private_key.pem'),
-  cert: fs.readFileSync('/etc/nginx/ssl/origin_certificate.pem'),
+ // key: fs.readFileSync('/etc/nginx/ssl/origin_private_key.pem'),
+ // cert: fs.readFileSync('/etc/nginx/ssl/origin_certificate.pem'),
   ca: fs.readFileSync('/etc/pki/tls/certs/ca-bundle.crt')
 };
 //ca: [fs.readFileSync('/etc/pki/tls/certs/ca-bundle.crt'),fs.readFileSync('/etc/pki/tls/certs/ca-bundle.crt')]
